7f23e4de261b66b71758da2b4521bd1321b20730
B Mathis <brandon@imathis.com>
2010-02-07 00:08:29 -0600

    fixed search templates, updated search form in navigation partial

--- source/javascripts/jquery.prettydate.js
+++ source/javascripts/jquery.prettydate.js
@@ -20,7 +20,7 @@ function prettyDate(time){
 			diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
 			diff < 7200 && "1 hour ago" ||
 			diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
-		day_diff == 1 && "Yesterday" ||
+		day_diff == 1 && "1 day ago" ||
 		day_diff < 7 && day_diff + " days ago" ||
 		day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago";
 }
