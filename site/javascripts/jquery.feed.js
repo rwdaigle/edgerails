8492077e73fa8a9511c777c7daa4a97e673051f3
Ryan Daigle <ryan.daigle@gmail.com>
2011-01-12 16:07:56 -0500

    Merge w/ deploy branch - no need to keep two branches

---
+++ site/javascripts/jquery.feed.js
@@ -0,0 +1,53 @@
+(function($) {
+  
+  // Fetch the feed
+  $.fn.feedme = function() {
+    
+    $(this).each(function(index, el) {
+      var $me = $(el);
+      var feedData = getStoredFeed($me.cookieName());
+    
+      if(!feedData) {
+        var feed = new google.feeds.Feed($me.attr('feed'));
+        feed.setNumEntries(5);
+        feed.load(function(result) {
+          storeFeed(result, $me.cookieName());
+          $me.applyFeed(result);
+        });
+      } else {
+        $me.applyFeed(feedData);
+      }      
+    });
+  };
+  
+  // Call back to process tweets and apply to template
+  $.fn.applyFeed = function(data) {
+    var $me = $(this);
+    $me.applyTemplate({    
+      getTemplateBody: function() { return $($me.attr('template')).html(); },
+      getData: function() { return data; }
+    });
+  };
+  
+  var COOKIE_NAME_SUFFIX = '_feed';
+  
+  // Call back to process tweets and apply to template
+  $.fn.cookieName = function() {
+    return $(this).attr('id') + COOKIE_NAME_SUFFIX
+  };
+  
+  var storeFeed = function(feed, cookieName) {
+    // $.cookie(cookieName, serializeFeed(feed), { expires: 1 })
+  };
+  
+  var getStoredFeed = function(cookieName) {
+    return null;
+    cookieData = $.cookie(cookieName);
+    return cookieData ? JSON.parse(cookieData) : null;
+  };
+  
+  var serializeFeed = function(feed) {
+    return JSON.stringify(feed, ['feed', 'entries', 'author', 'title', 'link', 'contentSnippet'])
+  }
+  
+}) (jQuery);
